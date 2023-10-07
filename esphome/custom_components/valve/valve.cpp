b74151bfb6ce7bed50e66aa192d565f6c6da3bc5
Alexander Evers <AEvers@lmt-group.com>
2023-10-07 11:46:46 +0200

    started custom component

--- esphome/custom_components/valve/valve.cpp
+++ esphome/custom_components/valve/valve.cpp
@@ -1,5 +1,9 @@
 #include "esphome/core/log.h"
 #include "valve.h"
+#include <cstdlib>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/adc.h>
 
 namespace esphome {
 namespace valve {
@@ -10,14 +14,60 @@ static const gpio_num_t LED_GPIO = static_cast<gpio_num_t>(15);
 TickType_t last_tick = 0;
 bool s_led_state = 0;
 
+static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
+{
+    BaseType_t mustYield = pdFALSE;
+    //Notify that ADC continuous driver has done enough number of conversions
+    vTaskNotifyGiveFromISR(s_task_handle, &mustYield);
+
+    return (mustYield == pdTRUE);
+}
+
+void valve::adc_init()
+{
+    adc_config = {
+        .max_store_buf_size = 1024,
+        .conv_frame_size = ADC_READ_LEN,
+    };
+    ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &adc_handle));
+
+    adc_continuous_config_t dig_cfg = {
+        .sample_freq_hz = ADC_SAMPLE_FREQ,
+        .conv_mode = ADC_CONV_SINGLE_UNIT_1,
+        .format = SOC_ADC_DIGI_MAX_BITWIDTH,
+    };
+
+    std::memset(&dig_cfg, 0, sizeof(dig_cfg));
+    dig_cfg.pattern_num = channel_num;
+    adc_pattern[0].atten = ADC_ATTEN_DB_0;
+    adc_pattern[0].channel = channel & 0x7;
+    adc_pattern[0].unit = ADC_UNIT_1;
+    adc_pattern[0].bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
+
+    ESP_LOGI(TAG, "adc_pattern[0].atten is :%"PRIx8, adc_pattern[0].atten);
+    ESP_LOGI(TAG, "adc_pattern[0].channel is :%"PRIx8, adc_pattern[0].channel);
+    ESP_LOGI(TAG, "adc_pattern[0].unit is :%"PRIx8, adc_pattern[0].unit);
+    
+    dig_cfg.adc_pattern = adc_pattern;
+    ESP_ERROR_CHECK(adc_continuous_config(handle, &dig_cfg));
+
+    adc_continuous_evt_cbs_t cbs = {
+        .on_conv_done = s_conv_done_cb,
+    };
+    ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));
+    ESP_ERROR_CHECK(adc_continuous_start(handle));
+}
+
+
 void Valve::setup() {
     ESP_LOGI(TAG, "Valve Setup GPIO %d as output", LED_GPIO);
-
 
     gpio_reset_pin(LED_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
     last_tick = xTaskGetTickCount();
+
+    adc_init();
 }
 
 void Valve::loop() {
--- esphome/custom_components/valve/valve.h
+++ esphome/custom_components/valve/valve.h
@@ -1,11 +1,21 @@
 #pragma once
 #include "esphome.h"
-#include "esphome/core/component.h"
 
 namespace esphome {
 namespace valve {
 
 class Valve : public Component {
+ protected:
+    static const uint32_t CONTROL_FREQ = 1000;
+    static const uint32_t ADC_SAMPLE_FREQ = 80000;
+    static const uint32_t ADC_READ_LEN = ADC_SAMPLE_FREQ / CONTROL_FREQ;
+
+    adc_channel_t adc_chan;
+    adc_continuous_handle_t adc_handle;
+    adc_continuous_handle_cfg_t adc_config;
+    adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX];
+
+    void adc_init();
  public:
   void setup() override;
   void loop() override;
