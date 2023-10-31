a2e4ae8504ab80678ca67340b99006c00b4f6afb
Alex "Tecnologic" Evers <tecnologic86@gmail.com>
2023-10-31 11:25:16 +0100

    started with 5.1.1 adc driver

--- esphome/custom_components/valve/__init__.py
+++ esphome/custom_components/valve/__init__.py
@@ -9,6 +9,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Valve)
 }).extend(cv.COMPONENT_SCHEMA)
 
+
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
--- esphome/custom_components/valve/valve.h
+++ esphome/custom_components/valve/valve.h
@@ -1,10 +1,12 @@
 #pragma once
-#include <cstdlib>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <driver/adc.h>
-#include "esphome.h"
-#include "esphome/core/log.h"
+#include <string.h>
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_adc/adc_continuous.h"
 
 namespace esphome
 {
@@ -14,9 +16,22 @@ namespace esphome
     class Valve : public Component
     {
     protected:
-      static constexpr uint32_t CONTROL_FREQ = 1000;
-      static constexpr uint32_t ADC_SAMPLE_FREQ = 8000;
+      static constexpr uint32_t CONTROL_FREQ = 10;
+      static constexpr uint32_t ADC_SAMPLE_FREQ = 400;
       static constexpr uint32_t ADC_READ_LEN = ADC_SAMPLE_FREQ / CONTROL_FREQ;
+      static constexpr uint32_t ADC_BUFFER_LEN = ADC_READ_LEN * 10;
+      static adc_channel_t channels_[9];
+      static uint8_t num_channels_;
+      static TaskHandle_t task_handle_;
+      static bool initialized;
+      static uint32_t adc_result;
+
+      static bool IRAM_ATTR Valve::adc_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data);
+      static void adc_init(void);
+
+      static void init(void);
+
+      esp_adc_cal_characteristics_t cal_characteristics_[SOC_ADC_ATTEN_NUM] = {};
 
     public:
       void setup() override;
