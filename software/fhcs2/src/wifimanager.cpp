8329e93c13cf4a6076b3ad56c0352c9526e05398
Alex "Tecnologic" Evers <tecnologic86@gmail.com>
2023-11-19 21:19:55 +0100

    got wifi manager code needs to be cleaned

--- software/fhcs2/src/wifimanager.cpp
+++ software/fhcs2/src/wifimanager.cpp
@@ -2,131 +2,125 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
-#include <DNSServer.h>
 #include "filesystem.h"
 
-namespace wifi
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Search for parameter in HTTP POST request
+const char *PARAM_INPUT_1 = "ssid";
+const char *PARAM_INPUT_2 = "pass";
+const char *PARAM_INPUT_3 = "ip";
+const char *PARAM_INPUT_4 = "gateway";
+
+// Variables to save values from HTML form
+String ssid;
+String pass;
+String ip;
+String gateway;
+
+// File paths to save input values permanently
+const char *ssidPath = "/ssid.txt";
+const char *passPath = "/pass.txt";
+const char *ipPath = "/ip.txt";
+const char *gatewayPath = "/gateway.txt";
+
+IPAddress localIP;
+
+// Set your Gateway IP address
+IPAddress localGateway;
+IPAddress subnet(255, 255, 0, 0);
+
+// Timer variables
+unsigned long previousMillis = 0;
+const long interval = 10000; // interval to wait for Wi-Fi connection (milliseconds)
+
+boolean restart = false;
+
+// Initialize WiFi
+bool initWiFi()
 {
-  // Create AsyncWebServer object on port 80
-  AsyncWebServer server(80);
-
-  // Search for parameter in HTTP POST request
-  constexpr char *PARAM_INPUT_1 = "ssid";
-  constexpr char *PARAM_INPUT_2 = "pass";
-  constexpr char *PARAM_INPUT_3 = "ip";
-  constexpr char *PARAM_INPUT_4 = "gateway";
-
-  // Variables to save values from HTML form
-  String ssid;
-  String pass;
-  String ip;
-  String gateway;
-
-  // File paths to save input values permanently
-  const char *ssidPath = "/ssid.txt";
-  const char *passPath = "/pass.txt";
-  const char *ipPath = "/ip.txt";
-  const char *gatewayPath = "/gateway.txt";
-
-  IPAddress localIP;
-
-  // Set your Gateway IP address
-  IPAddress localGateway;
-  IPAddress subnet(255, 255, 0, 0);
-
-  // Timer variables
-  unsigned long previousMillis = 0;
-  const long interval = 10000; // interval to wait for Wi-Fi connection (milliseconds)
-
-  // Initialize WiFi
-  bool initWiFi(void)
+  if (ssid == "" || ip == "")
   {
-    if (ssid == "" || ip == "")
-    {
-      Serial.println("Undefined SSID or IP address.");
-      return false;
-    }
-
-    WiFi.mode(WIFI_STA);
-    localIP.fromString(ip.c_str());
-    localGateway.fromString(gateway.c_str());
-
-    if (!WiFi.config(localIP, localGateway, subnet))
-    {
-      Serial.println("STA Failed to configure");
-      return false;
-    }
-    WiFi.begin(ssid.c_str(), pass.c_str());
-    Serial.println("Connecting to WiFi...");
-
-    unsigned long currentMillis = millis();
-    previousMillis = currentMillis;
-
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      currentMillis = millis();
-      if (currentMillis - previousMillis >= interval)
-      {
-        Serial.println("Failed to connect.");
-        return false;
-      }
-    }
-
-    Serial.println(WiFi.localIP());
-    return true;
+    Serial.println("Undefined SSID or IP address.");
+    return false;
   }
 
-  void init(void)
+  WiFi.mode(WIFI_STA);
+  localIP.fromString(ip.c_str());
+  localGateway.fromString(gateway.c_str());
+
+  if (!WiFi.config(localIP, localGateway, subnet))
   {
-    // Load values saved in SPIFFS
-    ssid = readFile(ssidPath);
-    pass = readFile(passPath);
-    ip = readFile(ipPath);
-    gateway = readFile(gatewayPath);
-    Serial.println(ssid);
-    Serial.println(pass);
-    Serial.println(ip);
-    Serial.println(gateway);
+    Serial.println("STA Failed to configure");
+    return false;
+  }
+  WiFi.begin(ssid.c_str(), pass.c_str());
 
-    if (initWiFi())
-    {
-      // Route for root / web page
-      server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-                { request->send(SPIFFS, "/index.html", "text/html", false, processor); });
-      server.serveStatic("/", SPIFFS, "/");
+  Serial.println("Connecting to WiFi...");
+  delay(20000);
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Failed to connect.");
+    return false;
+  }
 
-      // Route to set GPIO state to HIGH
-      server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
-                {
-      digitalWrite(ledPin, HIGH);
-      request->send(SPIFFS, "/index.html", "text/html", false, processor); });
+  Serial.println(WiFi.localIP());
+  return true;
+}
 
-      // Route to set GPIO state to LOW
-      server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
-                {
+void init()
+{
+  // Load values saved in LittleFS
+  ssid = filesystem::readFile(ssidPath);
+  pass = readFile(LittleFS, passPath);
+  ip = readFile(LittleFS, ipPath);
+  gateway = readFile(LittleFS, gatewayPath);
+  Serial.println(ssid);
+  Serial.println(pass);
+  Serial.println(ip);
+  Serial.println(gateway);
+
+  if (initWiFi())
+  {
+    // Route for root / web page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(LittleFS, "/index.html", "text/html", false, processor); });
+
+    server.serveStatic("/", LittleFS, "/");
+
+    // Route to set GPIO state to HIGH
+    server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
       digitalWrite(ledPin, LOW);
-      request->send(SPIFFS, "/index.html", "text/html", false, processor); });
-      server.begin();
-    }
-    else
-    {
-      // Connect to Wi-Fi network with SSID and password
-      Serial.println("Setting AP (Access Point)");
-      // NULL sets an open Access Point
-      WiFi.softAP("ESP-WIFI-MANAGER", NULL);
+      request->send(LittleFS, "/index.html", "text/html", false, processor); });
 
-      IPAddress IP = WiFi.softAPIP();
-      Serial.print("AP IP address: ");
-      Serial.println(IP);
+    // Route to set GPIO state to LOW
+    server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+      digitalWrite(ledPin, HIGH);
+      request->send(LittleFS, "/index.html", "text/html", false, processor); });
+    server.begin();
+  }
+  else
+  {
+    // Connect to Wi-Fi network with SSID and password
+    Serial.println("Setting AP (Access Point)");
+    // NULL sets an open Access Point
+    WiFi.softAP("ESP-WIFI-MANAGER", NULL);
 
-      // Web Server Root URL
-      server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-                { request->send(SPIFFS, "/wifimanager.html", "text/html"); });
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
 
-      server.serveStatic("/", SPIFFS, "/");
+    // Web Server Root URL
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(LittleFS, "/wifimanager.html", "text/html"); });
 
-      server.on("/", HTTP_POST, [](AsyncWebServerRequest *request)
-                {
+    server.serveStatic("/", LittleFS, "/");
+
+    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request)
+              {
       int params = request->params();
       for(int i=0;i<params;i++){
         AsyncWebParameter* p = request->getParam(i);
@@ -137,7 +131,7 @@ namespace wifi
             Serial.print("SSID set to: ");
             Serial.println(ssid);
             // Write file to save value
-            writeFile(SPIFFS, ssidPath, ssid.c_str());
+            writeFile(LittleFS, ssidPath, ssid.c_str());
           }
           // HTTP POST pass value
           if (p->name() == PARAM_INPUT_2) {
@@ -145,7 +139,7 @@ namespace wifi
             Serial.print("Password set to: ");
             Serial.println(pass);
             // Write file to save value
-            writeFile(SPIFFS, passPath, pass.c_str());
+            writeFile(LittleFS, passPath, pass.c_str());
           }
           // HTTP POST ip value
           if (p->name() == PARAM_INPUT_3) {
@@ -153,7 +147,7 @@ namespace wifi
             Serial.print("IP Address set to: ");
             Serial.println(ip);
             // Write file to save value
-            writeFile(SPIFFS, ipPath, ip.c_str());
+            writeFile(LittleFS, ipPath, ip.c_str());
           }
           // HTTP POST gateway value
           if (p->name() == PARAM_INPUT_4) {
@@ -161,15 +155,22 @@ namespace wifi
             Serial.print("Gateway set to: ");
             Serial.println(gateway);
             // Write file to save value
-            writeFile(SPIFFS, gatewayPath, gateway.c_str());
+            writeFile(LittleFS, gatewayPath, gateway.c_str());
           }
           //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
         }
       }
-      request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
-      delay(3000);
-      ESP.restart(); });
-      server.begin();
-    }
+      restart = true;
+      request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip); });
+    server.begin();
+  }
+}
+
+void loop()
+{
+  if (restart)
+  {
+    delay(5000);
+    ESP.restart();
   }
 }
